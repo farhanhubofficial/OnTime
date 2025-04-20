e00142e547cdc484bc3e18c672b0fa9f2eb7f1e8
farhanhubofficial <shaciye0757@gmail.com>
2025-04-20 15:58:35 +0300

    m

--- src/components/Approach.jsx
+++ src/components/Approach.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import CurtApproach from './images/approach.jpg'
+import CurtApproach from './images/Approach.jpg'
 import { Link } from 'react-router-dom'
 import { FaChevronRight } from "react-icons/fa6";
 
